c07db5b7dc6e43e0ab61db3c9fad7f71ce82b16c
Yury Zhelnin <yvzhelnin@gmail.com>
2016-09-30 23:09:16 +0500

    Добавлена отправка данных на сервер с помощью Ajax, реализован контроллер, принимающий и образабывающий получаемые данные. Пока - вывод на консоль.

---
+++ src/main/java/yz/feedback/SendController.java
@@ -0,0 +1,28 @@
+package yz.feedback;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class SendController {
+    
+    @RequestMapping(value="send", method=RequestMethod.POST)
+    public void sendFeedback(@RequestParam("secondName") String secondName,
+                        @RequestParam("firstName") String firstName,
+                        @RequestParam("patronymic") String patronymic,
+                        @RequestParam("recipient") String recipient,
+                        @RequestParam("topic") String topic,
+                        @RequestParam("message") String message){
+        
+        System.out.println(secondName);
+        System.out.println(firstName);
+        System.out.println(patronymic);
+        System.out.println(recipient);
+        System.out.println(topic);
+        System.out.println(message);
+    }
+}
